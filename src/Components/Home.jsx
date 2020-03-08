16c7fd3337ec056a1c290d93bde9a6984b8faf9a
Alexander Trout <alexandertrout@A1.local>
2020-03-08 19:35:57 +0000

    styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Menubar from "./Menu"
 import { StyledTop, OuterContainer } from "../styledComponents/styledComponents";
+import styled from 'styled-components';
 
-
+const HomePic = styled.img`
+  width: 50vw;
+`
 class Home extends Component {
   render() {
     return (
@@ -14,7 +17,7 @@ class Home extends Component {
         <OuterContainer>
           <h2> Hello welcome to the homepage </h2>
           <p> Text is a thing that should go here with stuff mapped out underneath for the rest of the page. Text is a thing that should go here with stuff mapped out underneath for the rest of the page. Text is a thing that should go here with stuff mapped out underneath for the rest of the page. Text is a thing that should go here with stuff mapped out underneath for the rest of the page. Text is a thing that should go here with stuff mapped out underneath for the rest of the page. </p>
-          <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjUxMTY3fQ&w=1000&q=80" alt="img" height="300" />
+          <HomePic src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjUxMTY3fQ&w=1000&q=80" alt="img"/>
         </OuterContainer>
       </main>
 
